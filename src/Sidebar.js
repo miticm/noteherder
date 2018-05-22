a1deae5d2c1af522739a670ff8fbd746c0ac762d
Tianchi Xu <xtc459924276@gmail.com>
2018-05-22 15:10:11 -0400

    add jsx to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
 const Sidebar = () => (
-    <div className="Sidebar">
-        Sidebar
+    <nav className="Sidebar">
+    <div className="logo">
+      <img src={quill} alt="Noteherder"/>
     </div>
+    <a className="new-note" href="/notes">
+      <img src={newHover} alt="New note"/>
+      <img className="outline" src={newIcon} alt="New note"/>
+    </a>
+    <div className="SignOut">
+      <button>
+        <i className="fa fa-sign-out"></i>
+      </button>
+    </div>
+  </nav>
 )
 
 export default Sidebar
