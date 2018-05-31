9105570cf92f511353f1de123cc62c9189d6e63e
Tianchi Xu <xtc459924276@gmail.com>
2018-05-30 20:09:04 -0400

    change readme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <Route component={App} />
-  </Router>, document.getElementById('root'));
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
